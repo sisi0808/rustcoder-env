d53ac9b4bec3be4b05e39d42f60a3df1ef40cbf6
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-01-24 20:43:29 +0900

    1B

--- abc055/src/bin/b.rs
+++ abc055/src/bin/b.rs
@@ -1,4 +1,15 @@
 use proconio::input;
 
 fn main() {
+    input! {
+        n: usize
+    }
+
+    let const_mod = 1_000_000_007;
+    let mut ans = 1;
+    for i in 1..=n {
+        ans *= i;
+        ans %= const_mod;
+    }
+    println!("{}", ans);
 }
